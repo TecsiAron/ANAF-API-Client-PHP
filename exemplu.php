5203a337de9f7d71c5a037b17fd216a533a34fa3
Aron <dukew@edu-soft.eu>
2024-02-01 14:52:02 +0200

    Update example (Logger is not a part of this package)

--- exemplu.php
+++ exemplu.php
@@ -2,7 +2,7 @@
 use EdituraEDU\ANAF\ANAFAPIClient;
 $Logger= function (string $message)
 {
-	Logger::getInstance()->WriteLog("orders", Logger::ERROR, $message, true);
+    echo $message;
 };
 $anaf = new ANAFAPIClient(ANAF_OAUTH,false, $Logger);
 var_dump($anaf->GetEntity("RO12345678"));
