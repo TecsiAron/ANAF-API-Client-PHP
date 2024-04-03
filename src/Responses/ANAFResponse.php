0fddba6c4460d0bc9f64c5466037ee109ea2e51b
Aron <dukew@edu-soft.eu>
2024-04-03 14:57:22 +0300

    Add utility function to amke error creation easier

--- src/Responses/ANAFResponse.php
+++ src/Responses/ANAFResponse.php
@@ -79,4 +79,15 @@ abstract class ANAFResponse
     {
         return $this->LastError === null;
     }
+
+    /**
+     * Utility method for implementers to create an error
+     * @param string $message
+     * @param int $code
+     * @return void
+     */
+    protected function CreateError(string $message, int $code= ANAFException::UNKNOWN_ERROR): void
+    {
+        $this->LastError = new ANAFException($message);
+    }
 }
